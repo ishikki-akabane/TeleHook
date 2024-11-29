c3c6d29a671cf7dfd3d48b85ba9751396b32e1e0
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-11-29 13:23:33 +0100

    Update __init__.py

--- telehook/__init__.py
+++ telehook/__init__.py
@@ -1,10 +1,10 @@
 __version__ = "0.0.2"
 
-from telehook.main import TeleClient, Filters
+#from telehook.main import TeleClient, Filters
 from telehook.testclient import testclient
  
 __all__ = [
-    "TeleClient",
-    "testclient",
-    "Filters"
+    #"TeleClient",
+    "testclient"
+    #"Filters"
 ]
