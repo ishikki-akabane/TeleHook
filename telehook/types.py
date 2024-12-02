a2c3236161d2aeea697bcf76477033b9e877b6d6
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-12-02 12:37:40 +0100

    Update types.py

--- telehook/types.py
+++ telehook/types.py
@@ -4,8 +4,8 @@ class User:
     def __init__(self, user_data):
         self.id = user_data.get('id')
         self.first_name = user_data.get('first_name')
-        self.last_name = user_data.get('last_name')
-        self.username = user_data.get('username')
+        self.last_name = user_data.get('last_name', None)
+        self.username = user_data.get('username', None)
 
 
 class Chat:
@@ -13,22 +13,22 @@ class Chat:
         self.id = chat_data.get('id')
         self.type = chat_data.get('type')
         self.title = chat_data.get('title')
-        self.username = chat_data.get('username')
+        self.username = chat_data.get('username', None)
 
 
-class Messageee:
+class Message:
     def __init__(self, client, message_data):
         self.client = client
         self.message_id = message_data.get('message_id')
         self.date = message_data.get('date')
         self.text = message_data.get('text')
-        self.chat = Chat(message_data.get('chat', {}))
-        self.from_user = User(message_data.get('from', {}))
+        #self.chat = Chat(message_data.get('chat', {}))
+        #self.from_user = User(message_data.get('from', {}))
 
     def reply_text(self, text):
-        self.client.send_message(self.chat.id, text)
+        self.client.send_message(7869684136, text)
 
-class Message:
+class Messageee:
     def __init__(self, client, data):
         self.client = client
         self.chat_id = data['chat']['id']
