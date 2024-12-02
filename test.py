be05617635db194ee554ac53448740a7c235407d
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-12-02 11:00:58 +0100

    Update test.py

--- test.py
+++ test.py
@@ -52,7 +52,7 @@ def webhook_endpoint():
 
 # ====================================================================
 
-#@TeleHook.on_message(Filters.command('start'), group=1)
+@TeleHook.on_message(Filters.command('start'))
 def start_cmd(client, message):
     url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
     payload = {
@@ -63,12 +63,6 @@ def start_cmd(client, message):
     response = requests.post(url, json=payload)
     user_info = message.from_user
     message.reply_text(f"hola, {user_info}")
-
-
-@on_message(TeleHook)
-def start_command(message):
-    if Filters.command('start')(message):
-        client.send_message(chat_id=message.chat_id, text="Hello! Welcome to our bot.")
 
 
 if __name__ == "__main__":
