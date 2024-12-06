c30634c314ba14188e2f4f52e53dea01be9fa83b
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-12-06 23:26:46 +0100

    Update test.py

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ def webhook_endpoint():
     try:
         update = request.get_json()
         text = f"```json\n{update}\n```"
-        requests.get(f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={text}&parse_mode=Markdown')
+        #requests.get(f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={text}&parse_mode=Markdown')
         TeleHook.process_update(update)
     except Exception as e:
         text = f"```python\nException: {e}\n```"
@@ -48,7 +48,17 @@ def handle_private_edit(bot, message):
         message.reply_text(f"Edited message in private chat: {message.text}")
     except Exception as e:
         requests.get(f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={e}')
-    
+
+
+@TeleHook.on_message(Filters.private)
+def read_message(bot, message):
+    try:
+        message.reply_text(f"{message.text}")
+    except Exception as e:
+        requests.get(f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={e}')
+
+
+
 
 if __name__ == "__main__":
     app.run()
