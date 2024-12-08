35c157a30307bb83210ae9f05d313ed606af61b8
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-12-08 06:15:48 +0100

    Update send_func.py

--- telehook/methods/send_func.py
+++ telehook/methods/send_func.py
@@ -23,3 +23,23 @@ class send_func:
                 return
         except Exception as e:
             return
+
+    def send_audio(self, chat_id, audio_content, filename="downloaded_audio.mp3"):
+        """
+        Send an audio file via the Telegram Bot API.
+
+        Args:
+            chat_id (int): The chat ID to send the audio to.
+            audio_content (bytes): The content of the audio file.
+            filename (str): The name of the file to be sent.
+        """
+        url = self.client.api_url + "sendAudio"
+        files = {'audio': (filename, audio_content)}
+        data = {'chat_id': chat_id}
+        
+        try:
+            response = requests.post(url, files=files, data=data)
+            if response.status_code != 200:
+                print(f"Failed to send audio: {response.text}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
