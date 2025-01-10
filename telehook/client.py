7cd413a7683ee182412897a5ae55543c2b1f36dd
Ishikki Akabane <rohansjaishwal@gmail.com>
2025-01-10 06:15:34 +0100

    Update client.py

--- telehook/client.py
+++ telehook/client.py
@@ -1,4 +1,5 @@
 
+import httpx
 import requests
 import logging
 import os
@@ -69,7 +70,7 @@ class TeleClient:
         else:
             return response.text
 
-    def process_update(self, update):
+    async def process_update(self, update):
         """
         Process an incoming update.
 
@@ -77,22 +78,16 @@ class TeleClient:
             update (dict): The Telegram webhook update.
         """
         if "message" in update:
-            try:
-                message = Message(self, update["message"])
-            except Exception as e:
-                logger.error(f'Error in responses: {e}')
+            message = Message(self, update["message"])
             for handler, filter_ in self.message_handlers:
-                if filter_(self, message):  # Here we pass client and message to the filter
-                    handler(self, message)
+                if await filter_(self, message):
+                    await handler(self, message)
 
         elif "edited_message" in update:
-            try:
-                edited_message = Message(self, update["edited_message"])
-            except Exception as e:
-                logger.error(f'Error in responses: {e}')
+            edited_message = Message(self, update["edited_message"])
             for handler, filter_ in self.edited_message_handlers:
-                if filter_(self, edited_message):
-                    handler(self, edited_message)
+                if await filter_(self, edited_message):
+                    await handler(self, edited_message)
 
 
     def on_message(self, filter_func):
