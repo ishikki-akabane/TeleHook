0bb5bde95858f73198328896c644604e6231c9c4
Ishikki Akabane <rohansjaishwal@gmail.com>
2025-01-12 05:57:47 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,51 +1,55 @@
 # testing
 
+import uvicorn
 
-from quart import Quart, request
+from fastapi import FastAPI, Request
 import requests
 from telehook import TeleClient, Filters
 
 
-BOT_TOKEN = "7612816971:AAFeh2njq6BcCEi-xTN5bLE7qKnAnzvvHMY"
-CHAT_ID = 7869684136
+BOT_TOKEN = "6599175207:AAG4Ow1nXH6LvQeQ-w8Pex6ZKJJ6BQ1WPz0"
 
-app = Quart(__name__)
+app = FastAPI()
+
 TeleHook = TeleClient(
     token=BOT_TOKEN,
-    url='https://telehook-test.vercel.app/webhook'
+    url='https://pup-solid-publicly.ngrok-free.app/webhook'
 )
 
-@app.route("/")
+@app.get("/")
 async def home_endpoint():
-    return "Telegram Webhook is running."
+    return {"message": "Telegram Bot is running."}
 
-@app.route('/webhook', methods=['POST'])
-async def webhook_endpoint():
+@app.post("/webhook")
+async def webhook_endpoint(request: Request):
     try:
-        update = request.get_json()
+        update = await request.json()
         await TeleHook.process_update(update)
     except Exception as e:
         print(e)
+        return {"error": str(e)}
+    return {"status": "ok"}
 
-    return 'ok'
-
-@app.route("/run")
-def run_endpoint():
-    result = TeleHook.setup_webhook()
-    return result
+@app.get("/run")
+async def run_endpoint():
+    try:
+        result = TeleHook.setup_webhook()
+        return {"webhook_setup_result": result}
+    except Exception as e:
+        return {"error": str(e)}
 
 # ====================================================================
 
-@TeleHook.on_message(Filters.command('start'))
+@TeleHook.on_edited(Filters.group)
 async def start_cmd(client, message):
     name = message.from_user.first_name
     try:
-        await message.reply_text("hellooo")
+        await message.reply_text(f"hellooo {name}")
     except Exception as e:
-        requests.get(f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={e}')
+        print(e)
 
 
 if __name__ == "__main__":
-    app.run()
+    uvicorn.run("test:app", port=5000, reload=True)
 
 
