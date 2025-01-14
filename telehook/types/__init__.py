53775b7ae2df439a0c09849979ee8f3e9e4343bc
Ishikki Akabane <rohansjaishwal@gmail.com>
2025-01-14 05:39:07 +0100

    Update __init__.py

--- telehook/types/__init__.py
+++ telehook/types/__init__.py
@@ -1,9 +1,10 @@
 # Types
 
-from telehook.types.message import Message
+from telehook.types.message import Message, CallbackQuery
 from telehook.types.keyboard import InlineKeyboardButton, InlineKeyboardMarkup
 
 __all__ = [
+    "CallbackQuery",
     "Message",
     "InlineKeyboardButton",
     "InlineKeyboardMarkup"
