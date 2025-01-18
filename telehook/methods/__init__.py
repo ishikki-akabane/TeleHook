20d93bd19d2e7e652e1d0e93990dd54a6c81f656
Ishikki Akabane <rohansjaishwal@gmail.com>
2025-01-18 06:24:21 +0100

    Update __init__.py

--- telehook/methods/__init__.py
+++ telehook/methods/__init__.py
@@ -3,11 +3,12 @@
 
 from .send_func import SendFunctions
 from .callback_func import CallbackFunctions
-
+from .edit_func import EditFunctions
 
 class Methods(
     SendFunctions,
-    CallbackFunctions
+    CallbackFunctions,
+    EditFunctions
 ):
     def __init__(self, client):
         self.client = client
