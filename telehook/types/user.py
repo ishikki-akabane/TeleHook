5858545d80e94dcdcba6760f8937f18c8eefd15d
Ishikki Akabane <rohansjaishwal@gmail.com>
2025-01-18 06:45:31 +0100

    Update user.py

--- telehook/types/user.py
+++ telehook/types/user.py
@@ -7,3 +7,6 @@ class User:
         self.first_name = user_data.get('first_name')
         self.last_name = user_data.get('last_name', None)
         self.username = user_data.get('username', None)
+
+    def __str__(self):
+        return f"User(id={self.id}, username='{self.username}', first_name='{self.first_name}')"
