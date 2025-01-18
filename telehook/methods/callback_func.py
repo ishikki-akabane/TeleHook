ea6bb41493a371cf34ca99e845886c56af2adb04
Ishikki Akabane <rohansjaishwal@gmail.com>
2025-01-18 05:23:43 +0100

    Create callback_func.py

---
+++ telehook/methods/callback_func.py
@@ -0,0 +1,32 @@
+
+import requests
+import httpx
+from typing import Union, Optional
+from ..types import InlineKeyboardButton, InlineKeyboardMarkup
+import json
+
+
+class CallbackFunctions:
+    async def answer_callback_query(
+            self,
+            callback_query_id,
+            text=None,
+            show_alert=False,
+            url=None,
+            cache_time=0
+        ):
+        """
+        Answer a callback query.
+        """
+        payload = {
+            "callback_query_id": callback_query_id,
+            "text": text,
+            "show_alert": show_alert,
+            "url": url,
+            "cache_time": cache_time
+        }
+        url = f"{self.client.api_url}answerCallbackQuery"
+        async with httpx.AsyncClient() as client:
+            response = await client.post(url, json=payload)
+            response.raise_for_status()
+            return response.json()
